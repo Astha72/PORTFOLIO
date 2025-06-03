26b0820162f485d1a4e92f03600553e067e564a2
astha Gautam <gautamastha836@gmail.com>
2025-06-03 11:34:58 +0530

    hello

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from './header';
+import Header from './Header';
 import About from './About';
 import Project from './Project';
 import { Route, Routes } from 'react-router-dom';
