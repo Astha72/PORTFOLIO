ecacf9f42400625d0460cdda524f889ad69c2523
astha Gautam <gautamastha836@gmail.com>
2025-06-05 02:43:42 +0530

    Updates

--- src/Components/LocationCard.jsx
+++
@@ -1,8 +0,0 @@
-function  LocationCard(){
-                return(
-                    <>
-                    <div className="w-full p-2 rounded-5xl"><img src="src\assets\location.jpg" alt="Our Location" /></div>
-                    </>
-                )
-            }
-            export default LocationCard;
